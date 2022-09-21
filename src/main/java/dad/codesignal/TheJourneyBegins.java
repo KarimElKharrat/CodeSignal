ef55f21546c4a2b2446c319b54d36c9fe3f5bd3b
KarimElKharrat <113608883+KarimElKharrat@users.noreply.github.com>
2022-09-21 09:14:16 +0100

    comentario en clase TheJourneyBegins

--- src/main/java/dad/codesignal/TheJourneyBegins.java
+++ src/main/java/dad/codesignal/TheJourneyBegins.java
@@ -2,10 +2,12 @@ package dad.codesignal;
 
 public class TheJourneyBegins {
 	
+	//ejercicio 1
 	public int add(int param1, int param2) {
 	    return param1 + param2;
 	}
 	
+	//ejercicio 2
 	public int centuryFromYear(int year) {
 		return year%100 == 0 ? year/100 : year/100+1;
 	}
